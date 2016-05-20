61176595f716dcd76f0a9d5c790a1dfff347ec85
Junho Kim <kuh9325@naver.com>
2016-05-20 13:26:34 +0900

    modify some data w/ re-refining

--- PID_Servo/PID.m
+++ PID_Servo/PID.m
@@ -12,46 +12,46 @@ time3_2 = time3_2 - time3_2(1);
 time3_3 = time3_3 - time3_3(1);
 
 figure(1)
-%time-angular velocity Diagram [P=0.8]
-plot(time1_1,velocity1_1,'LineWidth',1);
+%time-angle Diagram [P=0.8]
+plot(time1_1,angle1_1,'LineWidth',1);
 hold on
-title('time-angular velocity Diagram [variable P]','fontsize',12)
+title('time-angle Diagram [variable P]','fontsize',12)
 xlabel('Time (sec)','fontsize',12)
 ylabel('Angle (¡Æ)','fontsize',12)
 
-%time-angular velocity Diagram [P=1.0]
-plot(time1_2,velocity1_2,'LineWidth',1);
+%time-angle Diagram [P=1.0]
+plot(time1_2,angle1_2,'LineWidth',1);
 
-%time-angular velocity Diagram [P=1.2]
-plot(time1_3,velocity1_3,'LineWidth',1);
+%time-angle Diagram [P=1.2]
+plot(time1_3,angle1_3,'LineWidth',1);
 legend('P=0.8','P=1.0','P=1.2')
 
 figure(2)
-%time-angular velocity Diagram [P=1.0, D=0.005]
-plot(time2_1,velocity2_1,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.005]
+plot(time2_1,angle2_1,'LineWidth',1);
 hold on
-title('time-angular velocity Diagram [P=1.0, variable D]','fontsize',12)
+title('time-angle Diagram [P=1.0, variable D]','fontsize',12)
 xlabel('Time (sec)','fontsize',12)
 ylabel('Angle (¡Æ)','fontsize',12)
 
-%time-angular velocity Diagram [P=1.0, D=0.01]
-plot(time2_2,velocity2_2,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.01]
+plot(time2_2,angle2_2,'LineWidth',1);
 
-%time-angular velocity Diagram [P=1.0, D=0.03]
-plot(time2_3,velocity2_3,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.03]
+plot(time2_3,angle2_3,'LineWidth',1);
 legend('D=0.005','D=0.01','D=0.03')
 
 figure(3)
-%time-angular velocity Diagram [P=1.0, D=0.01, I=0.001]
-plot(time3_1,velocity3_1,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.01, I=0.001]
+plot(time3_1,angle3_1,'LineWidth',1);
 hold on
-title('time-angular velocity Diagram [P=1.0, D=0.01, variable I]','fontsize',12)
+title('time-angle Diagram [P=1.0, D=0.01, variable I]','fontsize',12)
 xlabel('Time (sec)','fontsize',12)
 ylabel('Angle (¡Æ)','fontsize',12)
 
-%time-angular velocity Diagram [P=1.0, D=0.01, I=0.002]
-plot(time3_2,velocity3_2,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.01, I=0.002]
+plot(time3_2,angle3_2,'LineWidth',1);
 
-%time-angular velocity Diagram [P=1.0, D=0.03, I=0.004]
-plot(time3_3,velocity3_3,'LineWidth',1);
+%time-angle Diagram [P=1.0, D=0.03, I=0.004]
+plot(time3_3,angle3_3,'LineWidth',1);
 legend('I=0.001','I=0.002','I=0.004')
--- PID_Servo/voltage.m
+++ PID_Servo/voltage.m
@@ -4,7 +4,7 @@ time1 = time1 - time1(1);
 
 %time-angle Diagram [PID]
 figure (1)
-plot(time1,angle1,'LineWidth',1);
-title('time-angle Diagram [changing voltage]','fontsize',12)
+plot(time1,velocity1,'LineWidth',1);
+title('time-angular velocity Diagram [changing voltage]','fontsize',12)
 xlabel('Time(sec)','fontsize',12)
 ylabel('Angular Velocity(RPM)','fontsize',12)
